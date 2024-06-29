b2fe249d3c6480271fd817b5507805d3142917a3
Micah" G. S <101915671+mintgumshoe@users.noreply.github.com>
2024-06-29 16:59:54 -0500

    Create utils.py
    Imports and configurations for the bot functions

---
+++ utils.py
@@ -0,0 +1,34 @@
+import random
+import datetime
+import time
+import asyncio
+import aiohttp
+import traceback
+import json
+import aiosqlite
+
+TOKEN = 'TOKEN'
+
+# ------------------------------ #
+###      Channel IDs           ###
+announce_channel = 0000000000000000000
+bot_channel = 0000000000000000000   
+mc_channel = 0000000000000000000
+
+starboard = 0000000000000000000
+react_board = 0000000000000000000
+bot_log = 0000000000000000000
+void_channel = 0000000000000000000
+embed_color = 0xffff00 
+
+# ------------------------------ #
+#       Additional Configs       #  
+
+starboard_minimum = 4
+currency = "🥥"
+maximum_bet = 500
+bank_name = "___ Credit Union"
+casino_name = "___ Casino"
+store_name = "_____ Mall"
+
+# ------------------------------ #
